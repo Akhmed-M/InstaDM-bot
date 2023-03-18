9361b353a6c1631c0c4d6377ce0ca1b9e6948daa
Ahmad <80991074+Akhmed-M@users.noreply.github.com>
2023-03-18 21:44:08 +0200

    Update myInfo.py

--- myInfo.py
+++ myInfo.py
@@ -6,9 +6,13 @@ my_password = ""
 
 # Other Info
 receiver_username = ""
-message = ""
+
+import random
+messages = [] # enter a list of messages
+message = random.choice(messages) # message chosen randomly from the list
+
 time = [,] # [hour, minute]
-
-
 # example: 10:00 PM
 # time = [22, 0] 
+
+# the message 'message' will be sent everyday at time 'time'
