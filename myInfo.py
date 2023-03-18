781f84c35cdb3840fe06c54a4a72f4fe77be0781
Ahmad <80991074+Akhmed-M@users.noreply.github.com>
2023-03-18 21:26:39 +0200

    Create myInfo.py

---
+++ myInfo.py
@@ -0,0 +1,14 @@
+
+# Your login Info
+my_username = ""
+my_password = ""
+
+
+# Other Info
+receiver_username = ""
+message = ""
+time = [,] # [hour, minute]
+
+
+# example: 10:00 PM
+# time = [22, 0] 
